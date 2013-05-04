5af7c3cadfd1d17353c549d63e2ae28e0bc9349b
Collin Jackson <jackson@apportable.com>
2013-05-03 19:13:35 -0700

    Change install.py to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/python
+/usr/bin/python <<DONE
 
 import os
 import tempfile
 
 sdk_url = 'https://s3.amazonaws.com/engineering-apportable/ApportableSDK/mac/f3e73d34678e04c5b6dd4fd7217f76bb41fa9db8/ApportableSDK-f3e73d34678e04c5b6dd4fd7217f76bb41fa9db8_ae563d5092e2371c.tgz'
+sdk_path = '/usr/local/apportable'
+cli_path = '/usr/local/bin/apportable'
 
 def download(url):
     output = os.path.join(tempfile.gettempdir(), url.split('/')[-1])
@@ -34,13 +36,16 @@ def install_sdk(path, destination):
     if err != 0:
         print "Install failed."
         exit(err)
+    print "Requesting root privileges to create " + cli_path
     err = os.system('sudo ln -s ' + os.path.join(destination, 'site_scons', 'apportable.py') + ' ' + '/usr/local/bin/apportable')
     if err != 0:
-        print "Tool install failed."
+        print "Unable to create " + cli_path
         exit(err)
 
 
 tarball = download(sdk_url)
 extracted = extract(tarball, os.path.join(tempfile.gettempdir()))
 update_toolchain(extracted)
-install_sdk(extracted, '/usr/local/ApportableSDK')
+install_sdk(extracted, sdk_path)
+
+DONE
