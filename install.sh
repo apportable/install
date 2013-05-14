e8344d122e17b0ae6261217d7f28e034f35d7b60
Collin Jackson <jackson@apportable.com>
2013-05-13 21:34:03 -0700

    add adb to path

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ echo "Toolchain updated."
 
 cd bin
 ln -s ../site_scons/apportable.py apportable
+ln -s ../toolchain/bin/macosx/android-sdk/platform-tools/adb adb
 
 echo "Apportable CLI is successfully installed at $SDK_PATH/bin/apportable"
 
